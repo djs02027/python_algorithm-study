7bc877a92d6142c0fb39452bf78c1cd0a3deb904
dbstnr2146 <dbstnr2146@naver.com>
2022-01-04 18:30:35 +0900

    0104

---
+++ 모각코/프로그래머스/level1/숫자와 문자열과 영단어/숫자와 문자열과 영단어.py
@@ -0,0 +1,9 @@
+def solution(s):
+    result = []
+    convertor = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+    for k, v in enumerate(convertor):
+        s = s.replace(v, str(k))
+
+    answer = int(s)
+    return answer
