40ce6ac8547637286fc667fc2622d95d3245af05
윤숙 <dbstnr2146@naver.com>
2022-01-17 23:33:55 +0900

    0117

--- solve.ac/class 2/4949_균형잡힌세상/4949_균형잡힌세상.py
+++ solve.ac/class 2/4949_균형잡힌세상/4949_균형잡힌세상.py
@@ -1,4 +1,42 @@
-import sys
 
-n=list(input().rstrip("\n"))
-print(n)
+
+import sys
+input = sys.stdin.readline
+
+
+while True:
+
+    text = input().rstrip()
+    stack = []
+    Flag = 1
+
+    if text == '.':
+        break
+
+    for t in text:
+        if t == '(' or t == '[':
+            stack.append(t)
+        elif t == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                stack.append(')')
+                flag = 0
+
+
+
+        elif t == ']':
+            if stack and stack[-1] == '[':
+                stack.pop()
+            else:
+                stack.append(']')
+                Flag = 0
+
+
+
+
+
+    print("yes" if Flag and not stack else "no")
+
+
+
