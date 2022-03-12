240941ed062d7893e944fc32b8b1457123ba194c
윤숙 <dbstnr2146@naver.com>
2022-03-12 23:20:32 +0900

    0312

---
+++ solve.ac/class 3/1107_리모컨/1107_리모컨.py
@@ -0,0 +1,12 @@
+Now=100
+cnt=0
+channer=input()
+breakN=int(input())
+breakButton=list(map(int,input().split()))
+button=[1]*10
+if Now-int(channer)<0:
+    op=0
+else:
+    op = abs(Now-int(channer))
+
+for i in range()
