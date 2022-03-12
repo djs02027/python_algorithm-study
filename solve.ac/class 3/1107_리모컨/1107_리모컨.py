88af9af4819b4fd5fe344314028cfaee401b905f
윤숙 <dbstnr2146@naver.com>
2022-03-13 02:37:40 +0900

    0312

--- solve.ac/class 3/1107_리모컨/1107_리모컨.py
+++ solve.ac/class 3/1107_리모컨/1107_리모컨.py
@@ -1,12 +1,24 @@
+
 Now=100
 cnt=0
 channer=input()
 breakN=int(input())
-breakButton=list(map(int,input().split()))
-button=[1]*10
-if Now-int(channer)<0:
-    op=0
+if breakN:
+    breakButton=list(input().split())
 else:
-    op = abs(Now-int(channer))
+    breakButton=[]
+button=[i for i in range(10)]
+op = abs(Now-int(channer))
 
-for i in range()
+#500000일때 +- 했을 경우 0에서 500000까지 이동범위와 1000000에서 이동범위를 갖게 조정해준다.
+for i in range(1000001):
+    text=str(i)
+    flag=0
+    for t in text:
+        if t in breakButton:
+            flag=1
+            break
+    if flag==0:
+
+        op=min(op,abs(int(text)-int(channer))+len(text))
+print(op)
