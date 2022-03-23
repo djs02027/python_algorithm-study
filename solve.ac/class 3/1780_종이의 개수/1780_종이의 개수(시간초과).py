9f9fc2bb68d38f9a016b94e8c72586111b68c325
윤숙 <dbstnr2146@naver.com>
2022-03-24 01:57:03 +0900

    0324

--- solve.ac/class 3/1780_종이의 개수/1780_종이의 개수(시간초과).py
+++ solve.ac/class 3/1780_종이의 개수/1780_종이의 개수(시간초과).py
@@ -6,11 +6,13 @@ def cutting(n, m, len_N):
     for i in range(0, n, len_N):
         first=True
         first_vlaue=None
-        dif_flag=False
-        Timeout=False
+
+
         for j in range(i, i + (len_N)):
+            dif_flag = False
+            Timeout = False
             for k in range(m, m + (len_N)):
-                #print(paper[j][k],j, k)
+                # print(paper[j][k],j, k)
                 if first==True:
                     first_vlaue=paper[j][k]
                     first=False
@@ -20,7 +22,6 @@ def cutting(n, m, len_N):
                         Timeout=True
                         break
             if Timeout==True:
-                Timeout=False
                 break
         if dif_flag==False:
             cac[int(first_vlaue)]+=1
@@ -29,14 +30,14 @@ def cutting(n, m, len_N):
                 for k in range(m, m + (len_N)):
                     cac[int(paper[j][k])]+=1
 
-
+        print(cac)
 
 N = int(input().rstrip())
 paper = [input().rstrip().split() for _ in range(N)]
 len_N = N
 cac=[0,0,0]
 while True:
-    if len_N == 3:
+    if len_N == 1:
         break
     len_N = len_N // 3
     for i in range(0, N, len_N):
--- solve.ac/class 3/1780_종이의 개수/1780_종이의 개수.py
+++ solve.ac/class 3/1780_종이의 개수/1780_종이의 개수.py
@@ -1,20 +1,47 @@
 def cutting(n, m, len_N):
-    n1, n2, n3 = 0, 0, 0
+    global cnt
+    visited = [0, 0, 0]
+
     for i in range(0, n, len_N):
         for j in range(i, i + (len_N)):
             for k in range(m, m + (len_N)):
-                print(j, k)
-        print("---")
+                visited[int(paper[j][k])] += 1
+        # print(visited)
+        flag = False
+        for v in range(len(visited)):
+            if visited[v] == (len_N*len_N):
+                cnt[v] += 1
+                flag = True
+                break
+        if flag == False:
+            cnt[0] += visited[0]
+            cnt[1] += visited[1]
+            cnt[2] += visited[2]
+        # print(cnt)
+        visited = [0, 0, 0]
+        # print("---")
+
+
 
 
 N = int(input())
 paper = [input().split() for _ in range(N)]
 len_N = N
-
+cnt = [0, 0, 0]
 #####
+breakflag=False
 while True:
-    if len_N == 3:
-        break
-    len_N = len_N // 3
+
+    # print(len_N)
+    if N>3:
+        len_N = len_N // 3
+    if len_N == 1:
+           break
     for i in range(0, N, len_N):
         cutting(N, i, len_N)
+    if N<=3:
+        len_N = len_N // 3
+
+print(cnt[2])
+print(cnt[0])
+print(cnt[1])
