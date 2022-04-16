a0f79bb8efd53b376ba14d1e8c1b65118822d9ca
윤숙 <dbstnr2146@naver.com>
2022-04-17 03:12:30 +0900

    0417

--- SWEA/Intermediate/파이썬 SW문제해결 기본 - Queue/피자 굽기/피자 굽기.py
+++ SWEA/Intermediate/파이썬 SW문제해결 기본 - Queue/피자 굽기/피자 굽기.py
@@ -1,11 +1,22 @@
+T = int(input())
+for tc in range(1, T + 1):
+    N, M = map(int, input().split())
+    txt = list(map(int, input().split()))
+    Pizzas = []
+    for i in range(len(txt)):
+        Pizzas.append([i+1, txt[i]])
 
-T=int(input())
-for tc in range(1,T+1):
-    N,M=map(int,input().split())
-    Pizzas=list(map(int,input().split()))
-
-    while True:
-        for i in range(N):
-            p
-
-
+    InOven = Pizzas[:N]
+    OutOven = Pizzas[N:]
+    # print(InOven)
+    # print(OutOven)
+    while len(InOven) != 1:
+        tmp = InOven.pop(0)
+        num, Now = tmp[0],tmp[1]
+        pizza = Now // 2
+        if pizza > 0:
+            InOven.append([num,pizza])
+        else:
+            if OutOven:
+                InOven.append(OutOven.pop(0))
+    print('#{} {}'.format(tc,InOven[0][0]))
