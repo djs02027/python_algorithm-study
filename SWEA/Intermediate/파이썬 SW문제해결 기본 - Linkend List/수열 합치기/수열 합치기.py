2cbf6db0852cbccb6a44fd64850f55a9d77fee1c
윤숙 <dbstnr2146@naver.com>
2022-04-20 22:53:55 +0900

    0420

--- SWEA/Intermediate/파이썬 SW문제해결 기본 - Linkend List/수열 합치기/수열 합치기.py
+++ SWEA/Intermediate/파이썬 SW문제해결 기본 - Linkend List/수열 합치기/수열 합치기.py
@@ -4,14 +4,7 @@ for tc in range(1, T + 1):
     txt = []
     for _ in range(M):
         arr = list(map(int, input().split()))
-        start = arr[0]
+        start = arr[0]-1
 
-        if txt:
-            cnt = 0
-            for a in arr:
-                txt.insert((start + cnt) - 1, a)
-                cnt += 1
-        else:
-            for a in arr:
-                txt.append(a)
+
     print(txt)
