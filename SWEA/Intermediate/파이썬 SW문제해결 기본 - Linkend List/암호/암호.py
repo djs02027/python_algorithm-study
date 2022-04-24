fc8e45ece02f4e78fbeaa70dce14c7a5c7077249
윤숙 <dbstnr2146@naver.com>
2022-04-24 23:40:42 +0900

    0424

--- SWEA/Intermediate/파이썬 SW문제해결 기본 - Linkend List/암호/암호.py
+++ SWEA/Intermediate/파이썬 SW문제해결 기본 - Linkend List/암호/암호.py
@@ -1,15 +1,22 @@
-T=int(input())
-for tc in range(1,T+1):
-    N,M,K=map(int,input().split())
-    numbers=list(map(int, input().split()))
-    step=M
-    for i in range(1,K+1):
-        step = M * i
-        if step >= len(numbers):
+T = int(input())
+for tc in range(1, T + 1):
+    N, M, K = map(int, input().split())
+    numbers = list(map(int, input().split()))
+    step = 0
+    for i in range(1, K + 1):
+        flag=False
+        step = step + M
+        if step > len(numbers):
             tmp = step - len(numbers)
             step = tmp
-        cnt=numbers[step-1]+numbers[step]
-        numbers[step:step]=[cnt]
+        elif step==len(numbers):
+            tmp = step - len(numbers)
+            flag=True
+        if flag==False:
+            cnt = numbers[step - 1] + numbers[step]
+            numbers[step:step] = [cnt]
+        else:
+            cnt = numbers[step-1] + numbers[0]
+            numbers.append(cnt)
 
-
-    print(numbers)
+    print('#{} {}'.format(tc, ' '.join(map(str,numbers[-10::][::-1]))))
