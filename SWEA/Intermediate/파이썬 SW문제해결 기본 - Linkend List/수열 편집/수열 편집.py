cd741acb1a82bc7cf6bc07ddcd0fae31ae236145
윤숙 <dbstnr2146@naver.com>
2022-04-27 02:09:12 +0900

    0427

--- SWEA/Intermediate/파이썬 SW문제해결 기본 - Linkend List/수열 편집/수열 편집.py
+++ SWEA/Intermediate/파이썬 SW문제해결 기본 - Linkend List/수열 편집/수열 편집.py
@@ -1,1 +1,18 @@
-#
+T=int(input())
+for tc in range(1,T+1):
+    N,M,L=map(int,input().split())
+    numbers=list(map(int,input().split()))
+
+    for _ in range(M):
+        txt=list(input().split())
+
+        if txt[0]=='I':
+            numbers[int(txt[1]):int(txt[1])]=[int(txt[2])]
+        elif txt[0]=='D':
+            numbers.pop(int(txt[1]))
+        elif txt[0]=='C':
+            numbers[int(txt[1])]=int(txt[2])
+    try:
+        print('#{} {}'.format(tc,numbers[L]))
+    except:
+        print('#{} -1'.format(tc))
