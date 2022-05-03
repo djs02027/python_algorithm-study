6421da41ef18598b52b70ac791b77129fbd4a111
윤숙 <dbstnr2146@naver.com>
2022-05-04 00:59:22 +0900

    0503

--- solve.ac/class 3/9095_1,2,3더하기/9095_1,2,3더하기.py
+++ solve.ac/class 3/9095_1,2,3더하기/9095_1,2,3더하기.py
@@ -1,7 +1,13 @@
-from itertools import permutations
+def DP(n):
+    if n==1:
+        return 1
+    elif n==2:
+        return 2
+    elif n==3:
+        return 4
+    else:
+        return DP(n-1)+DP(n-2)+DP(n-3)
 T=int(input())
 for tc in range(1,T+1):
     N=int(input())
-    for i in range(1,N+1):
-        print(N%i)
-
+    print(DP(N))
