946293c3951bfd099e97b50c939d905bb13e0112
윤숙 <dbstnr2146@naver.com>
2022-06-04 23:29:23 +0900

    0604

--- solve.ac/class 3/11403_경로찾기/11403_경로찾기.py
+++ solve.ac/class 3/11403_경로찾기/11403_경로찾기.py
@@ -1,3 +1,21 @@
-N=int(input())
-Field=[list(map(int,input().split())) for _ in range(N)]
-print(Field)
+from collections import deque
+
+
+def BFS(x, y):
+    visited[i][j]=1
+    visited[j][i]=1
+
+
+
+
+
+N = int(input())
+q = deque()
+Field = [list(map(int, input().split())) for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
+
+for i in range(N):
+    for j in range(N):
+        if Field[i][j] == 1:
+            BFS(i, j)
+print(visited)
