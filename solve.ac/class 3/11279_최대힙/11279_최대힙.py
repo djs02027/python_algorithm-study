14e8997f04b6bb60073c48f0024b1ac6083610a4
dbstnr2146 <dbstnr2146@naver.com>
2022-06-23 23:31:54 +0900

    0623

--- solve.ac/class 3/11279_최대힙/11279_최대힙.py
+++ solve.ac/class 3/11279_최대힙/11279_최대힙.py
@@ -1,3 +1,18 @@
-import  heapq
+import heapq
+import sys
 
-N=int(input())
+input = sys.stdin.readline
+N = int(input().rstrip())
+largehq = []
+
+for _ in range(N):
+    num = int(input().rstrip())
+    if num != 0:
+        heapq.heappush(largehq, -int(num))
+
+
+    if num == 0 and largehq:
+        print(-heapq.heappop(largehq))
+
+    elif num == 0 and not largehq :
+        print(0)
---
+++ solve.ac/class 3/11723_집합/11723_집합.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+S = set()
+for _ in range(N):
+    commend = input().rstrip()
+    commendList = commend.split()
+    if commendList[0] == 'add':
+        S.add(int(commendList[1]))
+
+    elif commendList[0] == 'check':
+        if int(commendList[1]) in S:
+            print(1)
+        else:
+            print(0)
+
+
+    elif commendList[0] == 'remove':
+        if int(commendList[1]) in S:
+            S.remove(int(commendList[1]))
+    elif commendList[0] == 'toggle':
+        if int(commendList[1]) in S:
+            S.remove(int(commendList[1]))
+        else:
+            S.add(int(commendList[1]))
+
+    elif commendList[0] == 'all':
+        pass
+
+    elif commendList[0] == 'empty':
+        S = set()
