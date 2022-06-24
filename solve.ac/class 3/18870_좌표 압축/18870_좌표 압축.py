998593a5ca397e63a7fdd2a0720e022ff4e2bece
dbstnr2146 <dbstnr2146@naver.com>
2022-06-24 23:59:29 +0900

    0624

---
+++ solve.ac/class 3/18870_좌표 압축/18870_좌표 압축.py
@@ -0,0 +1,16 @@
+#https://jason9319.tistory.com/356 참고
+#https://hongcoding.tistory.com/111
+import sys
+n = int(input())
+numbers = list(map(int, sys.stdin.readline().rstrip().split()))
+
+numset = set(numbers)
+a = list(numset)
+a.sort()
+
+numdict = {}
+for i in range(len(a)):
+    numdict[a[i]] = i
+
+for i in numbers:
+    print(numdict[i], end=' ')
