91462eaea191ff99074f4054d916945467e55aed
dbstnr2146 <dbstnr2146@naver.com>
2022-08-10 22:32:25 +0900

    0810

---
+++ 백준/코딩테스트 추천 문제/part2_복습/14888_연산자끼워넣기(DFS).py
@@ -0,0 +1,32 @@
+def DFS(step, total):
+    global add, sub, mul, div, minv, maxv
+    if step == N:
+        minv = min(minv, total)
+        maxv = max(maxv, total)
+    else:
+        if add > 0:
+            add -= 1
+            DFS(step + 1, total + numbers[step])
+            add += 1
+        if sub > 0:
+            sub -= 1
+            DFS(step + 1, total - numbers[step])
+            sub += 1
+        if mul > 0:
+            mul -= 1
+            DFS(step + 1, total * numbers[step])
+            mul += 1
+        if div > 0:
+            div -= 1
+            DFS(step + 1, total // numbers[step])
+            div += 1
+
+
+N = int(input())
+numbers = list(map(int, input().split()))
+add, sub, mul, div = map(int, input().split())
+minv = int(1e9)
+maxv = -int(1e9)
+DFS(1, numbers[0])
+print(maxv)
+print(minv)
